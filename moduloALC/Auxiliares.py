ab9745cdc3c487e1c34a9ca1d88d702760900191
L4u1337 <lautinesa@gmail.com>
2025-11-16 16:25:08 -0300

    Update Auxiliares.py

--- moduloALC/Auxiliares.py
+++ moduloALC/Auxiliares.py
@@ -35,3 +35,44 @@ def SonMatricesIguales(A,B,tol=1e-6):
             if abs(M[i][j]) > tol : return False
     
     return True
+
+
+
+
+#Esta funcion es necesaria para el punto 4 del TP, necesitamos eliminar la restriccion de m==n
+def QR_con_GS_MatRectangular(A, tol=1e-12, nops=False):
+    m, n = A.shape
+    
+    Q = np.zeros((m, n))
+    R = np.zeros((n, n))
+
+    operaciones = 0  
+
+    #Primera columna
+    Q[:,0] = A[:,0]/LABO3.norma(A[:,0],2)
+    R[0,0] = LABO3.norma(A[:,0],2)
+    operaciones += 4*m
+
+    for j in range(1,n):
+        aJ = A[:, j].copy()
+        
+        for k in range(j):
+            R[k, j] = LABO5.producto_interno(Q[:, k], A[:, j])
+            operaciones += 2*m
+            aJ = aJ - R[k, j] * Q[:, k]
+            operaciones += 2*m
+
+        R[j, j] = LABO3.norma(aJ, 2)
+        operaciones += 2*m
+
+        if abs(R[j, j]) < tol:
+            R[j, j] = 0
+            Q[:, j] = 0
+        else:
+            Q[:, j] = aJ / R[j, j]
+            operaciones += m
+
+    if nops:
+        return Q, R, operaciones
+    else:
+        return Q, R
